356e577505bd1ab7be837fad9a4cbe42c5eea21b
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2026-02-07 01:22:05 +0000

    added few more styling things

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -4,14 +4,14 @@ export const Card = ({ name, age, description, src }) => { // Corrected function
   return (
     /* 'row' class removed from here; col-12 col-md-4 ensures 3 columns on desktop */
     <div className="d-flex justify-content-center min-vh-200 bg-light col-sm-6 mt-4 mb-3 mb-sm-5 col-12 col-md-4">
-      <div className="Card text-center border border-secondary rounded-lg shadow-sm card-group p-4" style={{ width: '90%', maxWidth: '60rem' }}>
+      <div className="Card text-center border border-secondary rounded shadow-sm card-group p-4 w-75" >
         <div className="card-header d-flex justify-content-between align-items-center">
           <h5 className="card-title mb-0">{name}</h5>
           <p className="mb-0 ms-4">{age}</p>
         </div>
         <img 
           src={src || "https://via.placeholder.com/300x200?text=No+Image"} 
-          className="card-img-top object-fit-cover mt-3 mb-3 rounded" 
+          className="card-img-top object-fit-contain w-100 mt-3 mb-3 rounded" 
           alt="miami"
         />
         <div className="card-body">
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,33 +1,88 @@
 import React, { useState } from "react"; // Corrected imports
-import { Card } from "./Card"; 
-import { Navbar } from "./Navbar"; 
-import { Jumbotron } from "./Jumbotron"; 
-import { Footer } from "./Footer"; 
+import { Card } from "./Card";
+import { Navbar } from "./Navbar";
+import { Jumbotron } from "./Jumbotron";
+import { Footer } from "./Footer";
 
 //create your first component
 export const Home = () => {
-// i can just keep adding more item to this but make sure to also add it on line 26 for it to owrk
-  const [people, setPeople] = useState([
-    { name: "Alice", age: 30, Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.", src:"https://as2.ftcdn.net/v2/jpg/01/91/79/33/1000_F_191793364_6ZAWwd1nVmtIg6RK2NMy7eOiBdJH0hPw.jpg" },
-	{ name: "Bob", age: 25, Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.", src: "https://cdn.realisticasia.com/photos/Phong%20blogs/Bali%20Travel%20Guide/Nusa%20Panida,%20Bali.jpg"},
-	{ name: "Charlie", age: 35, Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.", src: "https://dynamic-media.tacdn.com/media/photo-o/2f/de/45/49/caption.jpg?f=webp&w=1000&h=700"},
-	{ name: "Diana", age: 28, Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.", src: "https://dynamic-media.tacdn.com/media/photo-o/30/4e/c2/b7/caption.jpg?f=webp&w=1000&h=700" },
-  { name: "Ethan", age: 32, Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.", src: "https://i0.wp.com/www.lonelyspeck.com/wp-content/uploads/2019/09/alabama-hills-at-moonrise.jpg?w=525&ssl=1" },
-  { name: "Fiona", age: 27, Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.", src: "https://as2.ftcdn.net/v2/jpg/01/32/62/81/1000_F_132628105_OBMh7kfO0JUqaDHJciEna0caOtAQSdah.jpg" },
+  // i can just keep adding more item to this but make sure to also add it on line 26 for it to owrk
+  const images = [
+    "https://as2.ftcdn.net/v2/jpg/01/91/79/33/1000_F_191793364_6ZAWwd1nVmtIg6RK2NMy7eOiBdJH0hPw.jpg",
+    "https://cdn.realisticasia.com/photos/Phong%20blogs/Bali%20Travel%20Guide/Nusa%20Panida,%20Bali.jpg",
+    "https://dynamic-media.tacdn.com/media/photo-o/2f/de/45/49/caption.jpg?f=webp&w=1000&h=700",
+    "https://dynamic-media.tacdn.com/media/photo-o/30/4e/c2/b7/caption.jpg?f=webp&w=1000&h=700",
+    "https://i0.wp.com/www.lonelyspeck.com/wp-content/uploads/2019/09/alabama-hills-at-moonrise.jpg?w=525&ssl=1",
+    "https://as2.ftcdn.net/v2/jpg/01/32/62/81/1000_F_132628105_OBMh7kfO0JUqaDHJciEna0caOtAQSdah.jpg",
+  ];
+  const pickRandomIndex = (array) => {
+    return Math.floor(Math.random() * array.length);
+  };
+  const [people] = useState([
+    {
+      name: "Alice",
+      age: 30,
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.",
+      src: images[pickRandomIndex(images)],
+    },
+    {
+      name: "Bob",
+      age: 25,
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.",
+      src: images[pickRandomIndex(images)],
+    },
+    {
+      name: "Charlie",
+      age: 35,
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.",
+      src: images[pickRandomIndex(images)],
+    },
+    {
+      name: "Diana",
+      age: 28,
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.",
+      src: images[pickRandomIndex(images)],
+    },
+    {
+      name: "Ethan",
+      age: 32,
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.",
+      src: images[pickRandomIndex(images)],
+    },
+    {
+      name: "Fiona",
+      age: 27,
+      Description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, ex.",
+      src: images[pickRandomIndex(images)],
+    },
   ]);
-  
+
   return (
     <div className="Home">
-		<Navbar />
-		<Jumbotron />
-        {/* This is the required "row" container that enables the 3-column layout */}
-        <div className="row"> 
-            {people.map((person, index) => (
-                <Card key={index} name={person.name} age={person.age} description={person.Description} src={(person.src)}/>
-            ))}
+      <Navbar />
+      <Jumbotron />
+      {/* This is the required "row" container that enables the 3-column layout */}
+      <div className="container bg-light">
+        <div className="row">
+          {people.map((person, index) => (
+            <Card
+              key={index}
+              name={person.name}
+              age={person.age}
+              description={person.Description}
+              src={person.src}
+            />
+          ))}
         </div>
-		<Footer /> 
-	</div>
+      </div>
+      <Footer />
+    </div>
   );
 };
 
